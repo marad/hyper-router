44764c282bc06d3ec106e459db6b518a09d4488c
Marcin Radoszewski <marcin.radoszewski@allegrogroup.com>
2015-08-12 11:01:55 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+extern crate hyper;
+extern crate rustc_serialize;
+
+mod router;
+
+use std::io;
+use hyper::server::{Server, Request, Response};
+use hyper::status::StatusCode;
+use hyper::method::Method::Get;
+use rustc_serialize::json;
+
+use router::{Route, Router, RouterBuilder};
+
+#[derive(RustcDecodable, RustcEncodable)]
+struct Person {
+    name: String,
+    age: i32,
+}
+
+fn request_handler(req: Request, mut res: Response) {
+        let person = Person { 
+            name: "Stefan".to_string(), 
+            age: 45,
+        };
+
+        res.send(&mut json::encode(&person).unwrap().as_bytes()).unwrap();
+
+        //io::copy(
+        //    &mut json::encode(&person).unwrap().as_bytes(),
+        //    &mut res.start().unwrap()
+        //).unwrap();
+}
+
+fn main() {
+    let router = RouterBuilder::new()
+        .add(Route {
+            method: Get,
+            path: "/hello".to_string(),
+            handler: request_handler
+        }).build();
+    
+    Server::http("0.0.0.0:8080").unwrap()
+        .handle(move |request: Request, response: Response| {
+            router.handle(request, response);
+        }).unwrap();
+}
---
+++ src/router.rs
@@ -0,0 +1,101 @@
+use hyper::method::Method;
+use hyper::method::Method::{Get, Post};
+use hyper::server::{Request, Response};
+use hyper::uri::RequestUri;
+use hyper::uri::RequestUri::AbsolutePath;
+use hyper::status::StatusCode;
+use std::fmt;
+
+pub type Handler = fn(Request, Response);
+
+pub struct Route {
+    pub method: Method,
+    pub path: String,
+    pub handler: Handler
+}
+
+impl fmt::Debug for Route {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Route {{method: {:?}, path: {:?}}}", self.method, self.path)
+    }
+}
+
+#[derive(Debug)]
+pub struct Router {
+    routes: Vec<Route>,
+}
+
+impl Router {
+    pub fn default_404_handler(request: Request, mut response: Response) {
+        {*response.status_mut() = StatusCode::NotFound}
+        response.send(b"page not found").unwrap();
+    }
+
+    pub fn handle(&self, request: Request, mut response: Response) {
+        let handler = self.find_handler(&request.uri);
+        handler(request, response);
+    }
+
+    fn find_handler(&self, uri: &RequestUri) -> Handler {
+        let copied_uri = uri.clone();
+        self.routes.iter().find(move |route| {
+                if let AbsolutePath(ref path) = copied_uri {
+                    path.clone() == route.path.clone()
+                } else {
+                    false
+                }
+            })
+            .map(|route| route.handler)
+            .unwrap_or(Router::default_404_handler)
+    }
+}
+
+#[derive(Debug)]
+pub struct RouterBuilder {
+    routes: Vec<Route>,
+}
+
+impl RouterBuilder {
+    pub fn new() -> RouterBuilder {
+        RouterBuilder { routes: vec![] }
+    }
+
+    pub fn add(mut self, route: Route) -> RouterBuilder {
+        self.routes.push(route);
+        self
+    }
+
+    pub fn build(self) -> Router {
+        Router { routes: self.routes }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use hyper::method::Method::{Get,Post};
+    use hyper::server::{Request, Response};
+    use super::{Route, RouterBuilder};
+
+    fn test_handler(req: Request, mut res: Response) {
+        res.send(b"Hello World").unwrap();
+    }
+
+    #[test]
+    fn test_api() {
+        let router = RouterBuilder::new()
+            .add(Route {
+                method: Get,
+                path: "/hello".to_string(),
+                handler: test_handler
+            })
+            .add(Route {
+                method: Post,
+                path: "/test".to_string(),
+                handler: test_handler
+            })
+            .build();
+
+        println!("{:?}", router);
+    }
+}
