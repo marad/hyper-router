54fa665d2720a7b66161a6a41e553a5c280d9ab8
Marcin Radoszewski <moriturius@gmail.com>
2016-05-23 20:14:02 +0000

    Added 0.1.2 documentation

---
+++ 0.1.2/hyper_router/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"mod":[["handlers",""],["route",""]],"struct":[["Path","Represents a path in HTTP sense (starting from `/`)"],["Route","Holds route information"],["RouteBuilder",""],["Router","This is the one. The router."],["RouterBuilder","Builder for a router"]],"type":[["Handler",""],["HttpResult",""]]});
