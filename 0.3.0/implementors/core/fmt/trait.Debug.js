050fe7a8fa830791a64c38441bcba6841f5f9ed4
Marcin Radoszewski <marcin.radoszewski@allegrogroup.com>
2017-03-29 15:26:12 +0200

    Added docs for version 0.3.0

---
+++ 0.3.0/implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["hyper_router"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='hyper_router/struct.Path.html' title='hyper_router::Path'>Path</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='hyper_router/struct.Route.html' title='hyper_router::Route'>Route</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='hyper_router/struct.RouterBuilder.html' title='hyper_router::RouterBuilder'>RouterBuilder</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='hyper_router/struct.Router.html' title='hyper_router::Router'>Router</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
