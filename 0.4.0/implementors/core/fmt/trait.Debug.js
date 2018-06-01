32807b97bb83564d44778e8ad2ccd23b42369a4b
Marcin Radoszewski <marcin.radoszewski@allegrogroup.com>
2018-06-01 12:19:43 +0200

    Published v0.4.0 documentation

---
+++ 0.4.0/implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["hyper_router"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"hyper_router/struct.Path.html\" title=\"struct hyper_router::Path\">Path</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"hyper_router/struct.Route.html\" title=\"struct hyper_router::Route\">Route</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"hyper_router/struct.RouterBuilder.html\" title=\"struct hyper_router::RouterBuilder\">RouterBuilder</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"hyper_router/struct.Router.html\" title=\"struct hyper_router::Router\">Router</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"hyper_router/struct.RouterService.html\" title=\"struct hyper_router::RouterService\">RouterService</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ 0.4.0/implementors/tokio_service/trait.Service.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["hyper_router"] = ["impl Service for <a class=\"struct\" href=\"hyper_router/struct.RouterService.html\" title=\"struct hyper_router::RouterService\">RouterService</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
