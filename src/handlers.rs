331f124bfe62604a31ada773ee48c1d627c70bb2
Marcin Radoszewski <moriturius@gmail.com>
2019-02-27 21:44:56 +0100

    Small refactor

--- src/handlers.rs
+++ src/handlers.rs
@@ -3,47 +3,29 @@ use hyper::{Request, Response, Body, StatusCode};
 
 pub fn default_404_handler(_: Request<Body>) -> Response<Body> {
     let body = "page not found";
-    Response::builder()
-        .status(StatusCode::NOT_FOUND)
-        .header(CONTENT_LENGTH, body.len() as u64)
-        .header(CONTENT_TYPE, "text/plain")
-        .body(Body::from(body))
-        .expect("Failed to construct response")
+    make_response(&body, StatusCode::NOT_FOUND)
 }
 
 pub fn method_not_supported_handler(_: Request<Body>) -> Response<Body> {
     let body = "method not supported";
-    Response::builder()
-        .status(StatusCode::METHOD_NOT_ALLOWED)
-        .header(CONTENT_LENGTH, body.len() as u64)
-        .header(CONTENT_TYPE, "text/plain")
-        .body(Body::from(body))
-        .expect("Failed to construct response")
+    make_response(&body, StatusCode::METHOD_NOT_ALLOWED)
 }
 
 pub fn internal_server_error_handler(_: Request<Body>) -> Response<Body> {
     let body = "internal server error";
-    Response::builder()
-        .status(StatusCode::INTERNAL_SERVER_ERROR)
-        .header(CONTENT_LENGTH, body.len() as u64)
-        .header(CONTENT_TYPE, "text/plain")
-        .body(Body::from(body))
-        .expect("Failed to construct response")
+    make_response(&body, StatusCode::INTERNAL_SERVER_ERROR)
 }
 
 pub fn not_implemented_handler(_: Request<Body>) -> Response<Body> {
     let body = "not implemented";
+    make_response(&body, StatusCode::NOT_IMPLEMENTED)
+}
+
+fn make_response(body: &'static str, status: StatusCode) -> Response<Body> {
     Response::builder()
-        .status(StatusCode::NOT_IMPLEMENTED)
+        .status(status)
         .header(CONTENT_LENGTH, body.len() as u64)
         .header(CONTENT_TYPE, "text/plain")
         .body(Body::from(body))
         .expect("Failed to construct response")
 }
-
-//fn make_response(body: &str, status: &StatusCode) {
-//    Response::new(Body::from(body))
-//        .with_status(status)
-//        .with_header(ContentLength(body.len() as u64))
-//        .with_header(ContentType::plaintext())
-//}
