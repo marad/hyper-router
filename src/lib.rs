475ba686dbb71d51e09088e49489462258db8515
Marko Kunic <kunicmarko20@gmail.com>
2019-06-24 19:00:30 +0100

    Fix wrong naming in docs

--- src/lib.rs
+++ src/lib.rs
@@ -27,7 +27,7 @@
 //! use hyper::rt::Future;
 //! use hyper_router::{Route, RouterBuilder, RouterService};
 //!
-//! fn request_handler(_: Request<Body>) -> Response<Body> {
+//! fn basic_handler(_: Request<Body>) -> Response<Body> {
 //!     let body = "Hello World";
 //!     Response::builder()
 //!         .header(CONTENT_LENGTH, body.len() as u64)
@@ -38,8 +38,8 @@
 //!
 //! fn router_service() -> Result<RouterService, std::io::Error> {
 //!     let router = RouterBuilder::new()
-//!         .add(Route::get("/hello").using(request_handler))
-//!         .add(Route::from(Method::PATCH, "/asd").using(request_handler))
+//!         .add(Route::get("/greet").using(basic_handler))
+//!         .add(Route::from(Method::PATCH, "/asd").using(basic_handler))
 //!         .build();
 //!
 //!     Ok(RouterService::new(router))
